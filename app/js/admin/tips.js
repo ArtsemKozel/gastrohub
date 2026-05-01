9018e664c527e335cec69b678e61b2cdcb80812a
Artchie <artsem86@gmail.com>
2026-05-01 08:50:20 +0200

    cleanup: remove debug log from tips.js

--- app/js/admin/tips.js
+++ app/js/admin/tips.js
@@ -150,8 +150,6 @@ async function loadTrinkgeld() {
         }
     }
 
-    console.log('dayResults 31.Mai', dayResults['2026-05-31']);
-
     // Tage rendern
     const daysContainer = document.getElementById('trinkgeld-days-list');
     if (allDates.length === 0) {
